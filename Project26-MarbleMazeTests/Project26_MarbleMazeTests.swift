98e062c9ed009fec3293b637856e1aa6bdc4cbec
Noah Pope <npope@tutanota.com>
2024-12-14 11:08:49 -0600

    Initial Commit

---
+++ Project26-MarbleMazeTests/Project26_MarbleMazeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Project26_MarbleMazeTests.swift
+//  Project26-MarbleMazeTests
+//
+//  Created by Noah Pope on 12/14/24.
+//
+
+import Testing
+@testable import Project26_MarbleMaze
+
+struct Project26_MarbleMazeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Project26-MarbleMazeUITests/Project26_MarbleMazeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Project26_MarbleMazeUITestsLaunchTests.swift
+//  Project26-MarbleMazeUITests
+//
+//  Created by Noah Pope on 12/14/24.
+//
+
+import XCTest
+
+final class Project26_MarbleMazeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
