62396bb41c2a14151ab775a2e3c8b6ecacd8aa10
Noah Pope <npope@tutanota.com>
2024-12-14 12:01:20 -0600

    adding enums, constants, and logic to load walls

--- Project26-MarbleMaze/GameScene.swift
+++ Project26-MarbleMaze/GameScene.swift
@@ -17,14 +17,44 @@ class GameScene: SKScene
     override func didMove(to view: SKView)
     {
         
-       
     }
     
     
     func loadLevel()
     {
-        guard let levelURL  = Bundle.main.url(forResource: "level1", withExtension: "txt")
-        else { fatalError("Could not find level1.txt in app bundle.") }
+        guard let levelURL      = Bundle.main.url(forResource: "level1", withExtension: "txt")
+        else { fatalError("Could not find level1.txt in the app bundle.") }
+        
+        guard let levelString   = try? String(contentsOf: levelURL)
+        else { fatalError("Could not load level1.txt from app budnle.") }
+        
+        let lines               = levelString.components(separatedBy: "\n")
+        for (row, line) in lines.reversed().enumerated()
+        {
+            for (column, letter) in line.enumerated()
+            {
+                let position    = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
+                if letter == "x"
+                {
+                    let node                            = SKSpriteNode(imageNamed: ImageNames.block)
+                    
+                    node.position                       = position
+                    node.physicsBody                    = SKPhysicsBody(rectangleOf: node.size)
+                    node.physicsBody?.categoryBitMask   = CollisionTypes.wall.rawValue
+                    node.physicsBody?.isDynamic         = false
+                    
+                    addChild(node)
+                }
+                else if letter == "v"
+                {
+                    
+                }
+                else if letter == "s" { /* load star */}
+                else if letter == "f" { /* load finish */}
+                else if letter == " " { /* empty space - do nothing */}
+                else { fatalError("Unknown level letter: \(letter)") }
+            }
+        }
     }
     
     
---
+++ Project26-MarbleMaze/Utilities/Constants+utils.swift
@@ -0,0 +1,22 @@
+//
+//  Constants+utils.swift
+//  Project26-MarbleMaze
+//
+//  Created by Noah Pope on 12/14/24.
+//
+
+import Foundation
+
+enum CollisionTypes: UInt32
+{
+    case player = 1
+    case wall   = 2
+    case star   = 4
+    case vortex = 8
+    case finish = 16
+}
+
+enum ImageNames
+{
+    static let block    = "block"
+}
